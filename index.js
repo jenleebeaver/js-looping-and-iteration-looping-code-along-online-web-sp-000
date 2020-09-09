f73ce1da4279e56b19afb0f862765f20f5557ec0
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-09-09 06:35:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
-const greeting = "suprise"
-const name = ['Lisa', 'Kaitlin', 'Jan']
+// const greeting = "suprise"
+// const name = ['Lisa', 'Kaitlin', 'Jan']
 
 function writeCards(name, greeting){
   for (let i = 0; i < name.length; i++){
