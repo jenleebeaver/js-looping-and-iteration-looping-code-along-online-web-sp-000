ffcfd1ddcf18a0471aeb14e48421d1a8b6716b99
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-09-09 06:37:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,15 @@
 // const greeting = "suprise"
 // const name = ['Lisa', 'Kaitlin', 'Jan']
 
+function writeCards(name, greeting){
+  // let thankYouCards = []
+  for (let i = 0; i < namesArray.length; i++){
+    console.log(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+  }
+  return thankYouCards
+}
+
+
 function writeCards(namesArray, event){
   let thankYouCards = []
   for (let i = 0; i < namesArray.length; i++){
