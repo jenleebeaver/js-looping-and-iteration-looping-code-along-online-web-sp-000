e3b80cbce9c36b0ca64a08bc03082ca9dcfb43b3
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-09-09 06:39:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
 // Code your solutions in this file
-// const greeting = "suprise"
-// const name = ['Lisa', 'Kaitlin', 'Jan']
 
-function writeCards(name, greeting){
-  // let thankYouCards = []
-  for (let i = 0; i < name.length; i++){
-    console.log(`Thank you, ${name[i]}, for the wonderful ${greeting} gift!`)
+function writeCards(namesArray, event){
+  let thankYouCards = []
+  for (let i = 0; i < namesArray.length; i++){
+    thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
   }
-  // return thankYouCards
+  return thankYouCards
 }
 
 
-// function writeCards(namesArray, event){
-//   let thankYouCards = []
-//   for (let i = 0; i < namesArray.length; i++){
-//     thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
-//   }
-//   return thankYouCards
-// }
-
-// function writeCards( namesArray, event ) {
-//   let thankYouCards = []
-//   for ( let i = 0; i < namesArray.length; i++ ) {
-//     thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
-//   }
-//   return thankYouCards
-// }
